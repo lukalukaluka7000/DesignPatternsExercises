4334c345e7e3ad4df696fb9d4b78e0b21fb582f3
Luka <luka.busic@fina.hr>
2021-03-03 14:22:19 +0100

    Exercise with bridge

---
+++ Adapter/adapter-exercise02/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace adapter_exercise02
+{
+    // Adapter Pattern Example            Judith Bishop Aug 2007
+    // Sets up a Coolbook
+    // This is D-J's as changed for the book
+    class AdapterPattern
+    {
+
+        //class SpaceBookSystem {  
+
+        public delegate void InputEventHandler(object sender, EventArgs e, string s);
+
+        //Adapter 
+        public class MyCoolBook : MyOpenBook
+        {
+            static SortedList<string, MyCoolBook> community =
+                     new SortedList<string, MyCoolBook>(100);
+            Interact visuals;
+
+            public MyCoolBook(string name) : base(name)
+            {
+                // create interact on the relavent thread, and start it!
+                new Thread(delegate () {
+                    visuals = new Interact("CoolBook Beta");
+                    visuals.InputEvent += new InputEventHandler(OnInput);
+                    visuals.FormClosed += new FormClosedEventHandler(OnFormClosed);
+                    Application.Run(visuals);
+                }).Start();
+                community[name] = this;
+
+                while (visuals == null)
+                {
+                    Application.DoEvents();
+                    Thread.Sleep(100);
+                }
+
+                Add("Welcome to CoolBook " + Name);
+            }
+
+            private void OnFormClosed(object sender, FormClosedEventArgs e)
+            {
+                community.Remove(Name);
+            }
+
+            private void OnInput(object sender, EventArgs e, string s)
+            {
+                Add("\r\n");
+                Add(s, "Poked you");
+            }
+
+            public new void Poke(string who)
+            {
+                Add("\r\n");
+                if (community.ContainsKey(who))
+                    community[who].Add(Name, "Poked you");
+                else
+                    Add("Friend " + who + " is not part of the community");
+            }
+
+            public new void Add(string message)
+            {
+                visuals.Output(message);
+            }
+
+            public new void Add(string friend, string message)
+            {
+                if (community.ContainsKey(friend))
+                    community[friend].Add(Name + " : " + message);
+                else
+                    Add("Friend " + friend + " is not part of the community");
+            }
+        }
+
+        //New Implementation (Adaptee)
+        public class Interact : Form
+        {
+            public TextBox Wall { get; set; }
+            public Button Poke { get; set; }
+
+            public Interact() { }
+
+            public Interact(string title)
+            {
+                Control.CheckForIllegalCrossThreadCalls = true;
+                Poke = new Button();
+                Poke.Text = "Poke";
+                this.Controls.Add(Poke);
+                Poke.Click += new EventHandler(Input);
+                Wall = new TextBox();
+                Wall.Multiline = true;
+                Wall.Location = new Point(0, 30);
+                Wall.Width = 300;
+                Wall.Height = 200;
+                Wall.Font = new Font(Wall.Font.Name, 12);
+                Wall.AcceptsReturn = true;
+                this.Text = title;
+                this.Controls.Add(Wall);
+            }
+
+            public event InputEventHandler InputEvent;
+
+            public void Input(object source, EventArgs e)
+            {
+                string who = Wall.SelectedText;
+                if (InputEvent != null)
+                    InputEvent(this, EventArgs.Empty, who);
+            }
+
+            public void Output(string message)
+            {
+                if (this.InvokeRequired)
+                    this.Invoke((MethodInvoker)delegate () { Output(message); });
+                else
+                {
+                    Wall.AppendText(message + "\r\n");
+                    this.Show();
+                }
+            }
+
+            protected override void OnFormClosed(FormClosedEventArgs e)
+            {
+                // Remove the interact and coolbook from the community here!
+                base.OnFormClosed(e);
+            }
+        }
+
+        // The RealSubject (ProxyPattern) 
+        // CANNOT CHANGE
+        public class SpaceBook
+        {
+            static SortedList<string, SpaceBook> community =
+                  new SortedList<string, SpaceBook>(100);
+            string pages;
+            string name;
+            string gap = "\n\t\t\t\t";
+
+            static public bool Unique(string name)
+            {
+                return community.ContainsKey(name);
+            }
+
+            internal SpaceBook(string n)
+            {
+                name = n;
+                community[n] = this;
+            }
+
+            internal string Add(string s)
+            {
+                pages += gap + s;
+                return gap + "======== " + name + "'s SpaceBook =========\n" +
+                      pages +
+                      gap + "\n===================================";
+            }
+
+            internal string Add(string friend, string message)
+            {
+                return community[friend].Add(message);
+            }
+
+            internal void Poke(string who, string friend)
+            {
+                community[who].pages += gap + friend + " poked you";
+            }
+        }
+
+        // Target (Adapter Pattern)
+        //CANNOT CHANGE
+        public class MyOpenBook
+        {
+
+            SpaceBook myOpenBook;
+            public string Name { get; set; }
+            public static int Users { get; set; }
+
+            public MyOpenBook(string n)
+            {
+                Name = n;
+                Users++;
+                myOpenBook = new SpaceBook(Name + "-" + Users);
+            }
+
+            public void Add(string message)
+            {
+                Console.WriteLine(myOpenBook.Add(message));
+            }
+
+            public void Add(string friend, string message)
+            {
+                Console.WriteLine(myOpenBook.Add(friend, Name + " : " + message));
+            }
+
+            public void Poke(string who)
+            {
+                myOpenBook.Poke(who, Name);
+            }
+
+            public void SuperPoke(string who, string what)
+            {
+                myOpenBook.Add(who, what + " you");
+            }
+        }
+
+        // The Client
+        static void Main()
+        {
+
+            MyCoolBook judith = new MyCoolBook("Judith");
+            judith.Add("Hello world");
+
+            MyCoolBook tom = new MyCoolBook("Tom");
+            tom.Poke("Judith");
+            tom.Add("Hey, We are on CoolBook");
+            judith.Poke("Tom");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Bridge/bridge-exercise01/Program.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge_exercise01
+{
+    // Bridge Pattern Example                           Judith Bishop Dec 2006, Aug 2007
+    // Extending SpaceBook with a second implementation via a Portal
+
+    // Abstraction  
+    class Portal
+    {
+        Bridge bridge;
+        public Portal(Bridge aSpaceBook)
+        {
+            bridge = aSpaceBook;
+        }
+        public void Add(string message)
+        { 
+            bridge.Add(message); 
+        }
+        public void Add(string friend, string message)
+        { 
+            bridge.Add(friend, message); 
+        }
+        public void Poke(string who)
+        {
+            bridge.Poke(who); 
+        }
+        public void MakeADeal()
+        {
+            //bridge.MakeADeal();
+            Console.WriteLine("Added method to portal, but not to bridge, so i dont have to impleemnt it everwhere.");
+        }
+    }
+
+    //Bridge
+    interface Bridge
+    {
+        void Add(string message);
+        void Add(string friend, string message);
+        void Poke(string who);
+        
+        //void MakeADeal(); //needs change in every bridge implementation
+    }
+
+    class SpaceBookSystem
+    {
+
+        // The Subject
+        private class SpaceBook
+        {
+            static SortedList<string, SpaceBook> community =
+                new SortedList<string, SpaceBook>(100);
+            string pages;
+            string name;
+            string gap = "\n\t\t\t\t";
+
+            static public bool IsUnique(string name)
+            {
+                return community.ContainsKey(name);
+            }
+
+            internal SpaceBook(string n)
+            {
+                name = n;
+                community[n] = this;
+            }
+
+            internal string Add(string s)
+            {
+                pages += gap + s;
+                return gap + "======== " + name + "'s SpaceBook =========" +
+                          pages + "\n" +
+                        gap + "================================";
+            }
+
+            internal string Add(string friend, string message)
+            {
+                return community[friend].Add(message);
+            }
+
+            internal void Poke(string who, string friend)
+            {
+                community[who].pages += gap + friend + " poked you";
+            }
+            internal void MakeADeal()
+            {
+                Console.WriteLine("MakeADeal in SpaceBook class");
+            }
+        }
+
+
+        // The Proxy (CANNOT CHANGE - we made a deal)
+        public class MySpaceBook// : Bridge
+        {
+            // Combination of a virtual and authentication proxy
+            SpaceBook mySpaceBook;
+            string password;
+            string name;
+            bool loggedIn = false;
+
+
+
+            void Register()
+            {
+                Console.WriteLine("Let's register you for SpaceBook");
+                do
+                {
+                    Console.WriteLine("All SpaceBook names must be unique");
+                    Console.Write("Type in a user name: ");
+                    name = Console.ReadLine();
+                } while (SpaceBook.IsUnique(name));
+                Console.Write("Type in a password: ");
+                password = Console.ReadLine();
+                Console.WriteLine("Thanks for registering with SpaceBook");
+            }
+
+            bool Authenticate()
+            {
+                Console.Write("Welcome " + name + ". Please type in your password: ");
+                string supplied = Console.ReadLine();
+                if (supplied == password)
+                {
+                    loggedIn = true;
+                    Console.WriteLine("Logged into SpaceBook");
+                    if (mySpaceBook == null)
+                        mySpaceBook = new SpaceBook(name);
+                    return true;
+                }
+                Console.WriteLine("Incorrect password");
+                return false;
+            }
+
+            public void Add(string message)
+            {
+                Check();
+                Console.WriteLine("Added to my wall through mySpaceBook");
+                if (loggedIn) mySpaceBook.Add(message);
+            }
+
+            public void Add(string friend, string message)
+            {
+                Check();
+                Console.WriteLine("Sent to wall through mySpaceBook");
+                if (loggedIn)
+                    Console.WriteLine(mySpaceBook.Add(friend, name + " said: " + message));
+            }
+            //public void MakeADeal()
+            //{
+            //    Check();
+            //    Console.WriteLine("Inside MySpaceBook : Bridge calling SpaceBook.MakeADeal");
+            //    mySpaceBook.MakeADeal();
+            //}
+
+            public void Poke(string who)
+            {
+                Check();
+                Console.WriteLine("Poked through mySpaceBook");
+                if (loggedIn)
+                    mySpaceBook.Poke(who, name);
+            }
+
+            void Check()
+            {
+                if (!loggedIn)
+                {
+                    if (password == null)
+                        Register();
+                    if (mySpaceBook == null)
+                        Authenticate();
+                }
+            }
+        }
+
+        // A Proxy with little to do
+        // Illustrates an alternative implementation in the Bridge pattern
+        public class MyOpenBook : Bridge
+        {
+            // Combination of a virtual and authentication proxy
+            SpaceBook myOpenBook;
+            string name;
+            static int users;
+
+            public MyOpenBook(string n)
+            {
+                name = n;
+                users++;
+                myOpenBook = new SpaceBook(name + "-" + users);
+            }
+
+            public void Add(string message)
+            {
+                Console.WriteLine(myOpenBook.Add(message));
+            }
+
+            public void Add(string friend, string message)
+            {
+                Console.WriteLine(myOpenBook.Add(friend, name + " : " + message));
+            }
+
+            public void Poke(string who)
+            {
+                myOpenBook.Poke(who, name);
+            }
+            public void MakeADeal()
+            {
+                Console.WriteLine("I made a deal with developers of SpaceBook (function: MakeADeal() in myOpenBook)");
+                Console.WriteLine("If I call this function just as is, won't work. In fact I can't even access this from client");
+                
+                Console.WriteLine("Now you cal call this. Calling SpaceBook.MakeADeal()");
+                myOpenBook.MakeADeal();
+            }
+            //compiler accept this, but how do we call this
+            //bcause in the main  program tom is referenced to Portal and SuperPoke is not in Portal
+            //solutions:
+            //1 add new operation to Portal, but not to Bridge, tako da ne treba svako implementirat
+            //2 cannot update Portal, create extension method to extend it
+            public void SuperPoke(string who, string what)
+            {
+                myOpenBook.Add(who, what + " you");
+            }
+        }
+        public class ExtendingSpaceSystemWithAnotherBridgeImplementations : Bridge
+        {
+            //TODO:
+            //want to implement new behaviour of add
+
+            //TODO:
+            //want to be able to use existing implementations in Portal or SpaceBook
+            public void Add(string message)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Add(string friend, string message)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Poke(string who)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    } // end class SpaceBookSystem
+
+    static class OpenBookExtensions
+    {
+        public static void SuperPoke(this Portal me, string who, string what)
+        {
+            me.Add(who, what + " you");
+        }
+    }
+
+    // The Client
+    class BridgePattern : SpaceBookSystem
+    {
+        static void Main()
+        {
+            Portal me = new Portal(new MyOpenBook("Judith"));
+            me.Add("Hello world");
+            me.Add("Today I worked 18 hours");
+            
+            
+
+            Portal tom = new Portal(new MyOpenBook("Tom"));
+            tom.Poke("Judith-1");
+            tom.SuperPoke("Judith-1", "hugged");
+            tom.Add("Judith-1", "Poor you");
+            tom.Add("Hey, I'm on OpenBook - it was so easy!");
+
+            Portal test = new Portal(new MyOpenBook("Test"));
+            test.MakeADeal();
+            
+            //Portal test2 = new Portal(new MySpaceBook());
+            //test2.MakeADeal();
+
+            Portal extended = new Portal(new ExtendingSpaceSystemWithAnotherBridgeImplementations());
+            extended.MakeADeal();
+
+            Console.ReadKey();
+        }
+    }
+    /* Output
+    Let's register you for SpaceBook
+    All SpaceBook names must be unique
+    Type in a user name: Judith
+    Type in a password: yey
+    Thanks for registering with SpaceBook
+    Welcome Judith. Please type in your password: yey
+    Logged into SpaceBook
+
+            ======== Judith's SpaceBook =========
+            Hello world
+            Today I worked 18 hours
+            Tom poked you
+            Tom : hugged you
+
+            ================================
+
+            ======== Judith's SpaceBook =========
+            Hello world
+            Today I worked 18 hours
+            Tom poked you
+            Tom : hugged you
+            Tom : Poor you
+
+            ================================
+
+            ======== Tom-1's SpaceBook =========
+            Hey, I'm on OpenBook - it was so easy!
+
+            ================================
+    */
+}
