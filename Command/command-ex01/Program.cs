315eddb6e4b09afa45cf241f1fa5ce34fab7af2b
lukalukaluka7000 <luka.busha-st3@hotmail.com>
2021-05-10 10:59:04 +0200

    initing new theory command example

---
+++ Command/command-ex01/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class CommandPattern
+{
+    // Command Pattern           Judith Bishop June 2007
+    //
+    // Uses a single delegate for the single type of commands that the client invokes.
+
+    delegate void Invoker();
+    static Invoker Execute, Undo, Redo;
+
+    class Command
+    {
+        public Command(Receiver receiver)
+        {
+            Execute = receiver.Action;
+            Redo = receiver.Action;
+            Undo = receiver.Reverse;
+        }
+    }
+
+    public class Receiver
+    {
+        string build, oldbuild;
+        string s = "some string ";
+
+        public void Action()
+        {
+            oldbuild = build;
+            build += s;
+            Console.WriteLine("Receiver is adding " + build);
+        }
+
+        public void Reverse()
+        {
+            build = oldbuild;
+            Console.WriteLine("Receiver is reverting to " + build);
+        }
+    }
+
+    static void Main()
+    {
+        new Command(new Receiver());
+        Execute();
+        Redo();
+        Undo();
+        Execute();
+        Console.ReadKey();
+    }
+}
+/* Output
+Receiver is adding some string 
+Receiver is adding some string some string 
+Receiver is reverting to some string 
+Receiver is adding some string some string
+*/
+
+
